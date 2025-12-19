3c942c8fc7d838b4227d7d313f3b23b1e3b8dde1
admin <shimlyqaq@163.com>
2025-12-19 08:31:13 +0800

    项目初始化

---
+++ gulimall-wara/src/main/java/com/itguigu/gulimall/wara/GulimallWaraApplication.java
@@ -0,0 +1,13 @@
+package com.itguigu.gulimall.wara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWaraApplication.class, args);
+    }
+
+}
